6fb49e8b767dc737257c92d08c7465a439981e64
あお <113911070+nlaocs@users.noreply.github.com>
2024-08-09 22:42:25 +0900

    printlnする際の表記法をスネークケースからタイトルケースに変更

--- src/main.rs
+++ src/main.rs
@@ -163,11 +163,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             user_data.id_to_link(ImageType::AvatarDecoration)
         );
 
-        println!("id: {}", user_data.id);
-        println!("username: {}", user_data.username);
-        println!("avatar: {}", avatar?);
-        println!("discriminator: {}", user_data.discriminator);
-        println!("public_flags: {}", user_data.public_flags);
+        println!("ID: {}", user_data.id);
+        println!("Username: {}", user_data.username);
+        println!("Avatar: {}", avatar?);
+        println!("Discriminator: {}", user_data.discriminator);
+        println!("Public Flags: {}", user_data.public_flags);
         if user_data.public_flags != 0 {
             println!("Badge:");
             for flag in user_data.check_flags() {
@@ -176,38 +176,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         } else {
             println!("Badge: None");
         }
-        println!("flags: {}", user_data.flags);
-        println!("bot: {}", user_data.bot.unwrap_or_else(|| false));
-        println!("banner: {}", banner?);
+        println!("Flags: {}", user_data.flags);
+        println!("Bot: {}", user_data.bot.unwrap_or_else(|| false));
+        println!("Banner: {}", banner?);
         if user_data.accent_color.is_some() {
-            println!("accent_color: {}", format!("#{:06x}", user_data.accent_color.unwrap()));
+            println!("Accent Color: {}", format!("#{:06x}", user_data.accent_color.unwrap()));
         } else {
-            println!("accent_color: None");
+            println!("Accent Color: None");
         }
-        println!("global_name: {}", user_data.global_name.unwrap_or_else(|| "None".to_string()));
+        println!("Global Name: {}", user_data.global_name.unwrap_or_else(|| "None".to_string()));
         if user_data.avatar_decoration_data.is_some() {
             let avatar_decoration_data = user_data.avatar_decoration_data.clone().unwrap();
-            println!("avatar_decoration_data:");
-            println!(" - asset: {}", asset?);
-            println!(" - sku_id: {}", avatar_decoration_data.sku_id);
+            println!("Avatar Decoration Data:");
+            println!(" - Asset: {}", asset?);
+            println!(" - SKU ID: {}", avatar_decoration_data.sku_id);
             if avatar_decoration_data.expires_at.is_some() {
-                println!(" - expires_at: {}", avatar_decoration_data.expires_at.unwrap());
+                println!(" - Expires at: {}", avatar_decoration_data.expires_at.unwrap());
             } else {
-                println!(" - expires_at: None");
+                println!(" - Expires at: None");
             }
         } else {
-            println!("avatar_decoration_data: None");
+            println!("Avatar Decoration Data: None");
         }
-        println!("banner_color: {}", user_data.banner_color.unwrap_or_else(|| "None".to_string()));
+        println!("Banner Color: {}", user_data.banner_color.unwrap_or_else(|| "None".to_string()));
         if user_data.clan.is_some() {
             let clan = user_data.clan.clone().unwrap();
-            println!("clan:");
-            println!(" - identity_guild_id: {}", clan.identity_guild_id);
-            println!(" - identity_enabled: {}", clan.identity_enabled);
-            println!(" - tag: {}", clan.tag);
-            println!(" - badge: {}", clan.badge);
+            println!("Clan:");
+            println!(" - Identity Guild Id: {}", clan.identity_guild_id);
+            println!(" - Identity Enabled: {}", clan.identity_enabled);
+            println!(" - Tag: {}", clan.tag);
+            println!(" - Badge: {}", clan.badge);
         } else {
-            println!("clan: None");
+            println!("Clan: None");
         }
         println!();
     }
